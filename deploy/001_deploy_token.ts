573ab2b909e42adb39a961de200b3334c194e3aa
zhangtzDH <72637059+zhangtzDH@users.noreply.github.com>
2022-11-23 16:14:02 +0800

    ADD

--- deploy/001_deploy_token.ts
+++ deploy/001_deploy_token.ts
@@ -5,13 +5,21 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
   const {deploy} = deployments;
 
+  console.log(deploy.toString)
+
   const {deployer, tokenOwner} = await getNamedAccounts();
 
+  console.log(deployer)
+
+  console.log(tokenOwner)
   await deploy('Token', {
     from: deployer,
     args: [tokenOwner],
     log: true,
   });
+ 
 };
 export default func;
 func.tags = ['Token'];
+
+
